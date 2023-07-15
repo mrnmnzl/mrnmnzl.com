74b15d93451a4dcaff6c2e2324d8f97b2ff0a75b
Marion Menzl <marion.menzl@me.com>
2023-07-15 19:12:11 +0200

    Make desktop clickable even with window open

--- components/desktop/index.js
+++ components/desktop/index.js
@@ -1,18 +1,13 @@
-"use client";
 import React from "react";
-import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Desktop = ({ children }) => {
-  const path = usePathname();
-  const isHomepage = path === "/" ? "hidden" : "block";
-  const classes =
-    "w-full absolute top-0 sm:top-[25px] z-50 sm:z-0 h-full sm:h-[calc(100%-100px)] " +
-    isHomepage;
-
   return (
     <div className="flex-1 p-[30px 15px] sm:p-[15px]">
-      <div className="flex content-start sm:justify-start flex-wrap sm:flex-col sm:flex-wrap-reverse w-full h-full"></div>
-      <div className={classes}>{children}</div>
+      <div className="flex content-start sm:justify-start flex-wrap sm:flex-col sm:flex-wrap-reverse w-full h-full">
+        <Link href="/settings">⚙️</Link>
+        {children}
+      </div>
     </div>
   );
 };
