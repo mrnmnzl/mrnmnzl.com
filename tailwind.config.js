7828843dce36edc3c550667e6f6b3bb387c970c9
Marion Menzl <marion.menzl@me.com>
2023-07-15 19:13:29 +0200

    Add more custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        dot: "inset 0px 0px 0px 1px rgba(0,0,0,0.07)",
+        window: "0px 11px 33px rgba(0, 0, 0, 0.2)",
+      },
       colors: {
+        "button-light": "rgba(214, 214, 214, 1)",
+        "button-dark": "rgba(101, 101, 101, 1)",
         "menu-bar": "rgba(255, 255, 255, 0.8)",
-        "menu-bar-text": "rgba(239	237	243	)",
+        "menu-bar-text": "rgba(239, 237, 243, 1)",
         "menu-bar-dark": "rgba(0, 0, 0, 0.1)",
         "menu-bar-border": "rgba(161, 161, 161, 0.2)",
         dock: "rgba(255, 255, 255, 0.4)",
         "dock-dark": "rgba(158, 158, 158, 0.2)",
         "dock-mobile": "rgba(255, 255, 255, 0.4)",
         "dock-mobile-dark": "rgba(0,0,0, 0.3)",
+        "text-light": "rgba(72, 72, 72, 1)",
+        "text-dark": "rgba(233, 233, 233, 1)",
+        "window-light": "rgba(240, 240, 240, 1)",
+        "window-dark": "rgba(40, 40, 40, 1)",
       },
     },
   },
