99bdc86c7b89980bf0db69e3d5103c4ab08ff942
Marion Menzl <marion.menzl@me.com>
2023-07-15 19:09:42 +0200

    Add dots components

---
+++ components/dots/dot.js
@@ -0,0 +1,29 @@
+// Single dot component
+import React from "react";
+
+const Dot = ({ location, active, red, yellow, green }) => {
+  function renderDot() {
+    if (red) {
+      return active ? "bg-red-400" : "group-hover:bg-red-400";
+    } else if (yellow) {
+      return active ? "bg-yellow-400" : "group-hover:bg-yellow-400";
+    } else if (green) {
+      return active ? "bg-green-400" : "group-hover:bg-green-400";
+    } else {
+      return "";
+    }
+  }
+
+  return red || yellow || green ? (
+    <a
+      href={location}
+      className={
+        "h-[12px] w-[12px] rounded-2xl mr-[8px] shadow-dot " + renderDot()
+      }
+    ></a>
+  ) : (
+    <div className="h-[12px] w-[12px] rounded-2xl mr-[8px] shadow-dot"></div>
+  );
+};
+
+export default Dot;
---
+++ components/dots/index.js
@@ -0,0 +1,15 @@
+// The three dots that appear in the top right corner of the screen
+import React from "react";
+import Dot from "./dot";
+
+const Dots = ({ location, red, yellow, green, activeDots }) => {
+  return (
+    <div className="group flex">
+      <Dot red={red} location={location} active={!!activeDots} />
+      <Dot yellow={yellow} location={location} active={activeDots > 1} />
+      <Dot green={green} location={location} active={activeDots > 2} />
+    </div>
+  );
+};
+
+export default Dots;
