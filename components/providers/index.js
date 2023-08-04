e9dcb482a578e6fa879ff3e66fdb8d174586e71f
Marion Menzl <marion.menzl@me.com>
2023-08-04 22:22:24 +0200

    Add theme provider

---
+++ components/providers/index.js
@@ -0,0 +1,8 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+
+function Providers({ children }) {
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+}
+
+export default Providers;
