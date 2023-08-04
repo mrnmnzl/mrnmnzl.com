e0aeb3e8f3d60f8e5bbe6e7b73186e6c8c6eac5c
Marion Menzl <marion.menzl@me.com>
2023-08-04 22:23:22 +0200

    Add provider to html

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import MenuBar from "@/components/menu-bar";
 import Desktop from "@/components/desktop";
 import Dock from "@/components/dock";
+import Providers from "@/components/providers";
 import "./globals.css";
 
 export const metadata = {
@@ -12,9 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="absolute top-0 flex flex-col w-full h-full">
-        <MenuBar title={metadata.title} />
-        <Desktop>{children}</Desktop>
-        <Dock />
+        <Providers>
+          <MenuBar title={metadata.title} />
+          <Desktop>{children}</Desktop>
+          <Dock />
+        </Providers>
       </body>
     </html>
   );
