6a303c687811b4d4638bd11c719b001816a43afd
Marion Menzl <marion.menzl@me.com>
2023-09-01 13:03:28 +0200

    Add sanity to project

---
+++ schemas/documents/project.js
@@ -0,0 +1,72 @@
+import { BsCodeSquare } from "react-icons/bs";
+import { defineField } from "sanity";
+
+const project = {
+  name: "project",
+  title: "Project",
+  description: "Project Schema",
+  type: "document",
+  icon: BsCodeSquare,
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: "Enter the name of the project",
+    },
+    defineField({
+      name: "tagline",
+      title: "Tagline",
+      type: "string",
+      validation: (rule) => rule.max(60).required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      description:
+        "Add a custom slug for the URL or generate one from the name",
+      options: { source: "name" },
+      validation: (rule) => rule.required(),
+    }),
+    {
+      name: "logo",
+      title: "Project Logo",
+      type: "image",
+    },
+    {
+      name: "projectUrl",
+      title: "Project URL",
+      type: "url",
+    },
+    {
+      name: "coverImage",
+      title: "Cover Image",
+      type: "image",
+      description: "Upload a cover image for this project",
+      options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      description: "Write a full description about this project",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "date",
+      title: "Date",
+      type: "date",
+      description: "The date this project was completed",
+    },
+  ],
+};
+
+export default project;
---
+++ schemas/index.js
@@ -0,0 +1,3 @@
+import project from "./documents/project";
+
+export const schema = { types: [project] };
