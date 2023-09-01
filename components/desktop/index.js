6a05abdac9d27139069f0f8e3cef062da5b08455
Marion Menzl <marion.menzl@me.com>
2023-09-01 13:04:20 +0200

    Make desktop work for fullscreen pages

--- components/desktop/index.js
+++ components/desktop/index.js
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 const Desktop = ({ children }) => {
   return (
-    <div className="flex-1 p-[30px 15px] sm:p-[15px]">
-      <div className="flex flex-wrap content-start w-full h-full sm:justify-start sm:flex-col sm:flex-wrap-reverse">
-        <Link href="/settings">⚙️</Link>
-        {children}
+    <div className="relative flex-1">
+      <div className=" p-[30px 15px] sm:p-[15px]">
+        <div className="flex flex-wrap content-start w-full h-full sm:justify-start sm:flex-col sm:flex-wrap-reverse">
+          <Link href="/settings">⚙️</Link>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
