dd834acd6b7086d28d66b423dd6d29ae050153c5
Marion Menzl <marion.menzl@me.com>
2023-09-15 12:52:37 +0200

    Add download button for cv

--- app/cv/page.js
+++ app/cv/page.js
@@ -1,6 +1,7 @@
 import { Dots } from "@/components/Dots";
 import { fetchCV } from "@/sanity/utils/fetchCV";
 import { PDFRenderer } from "@/components/PDFRenderer";
+import { LuExternalLink, LuDownload } from "react-icons/lu";
 
 async function CVPage() {
   const cvData = await fetchCV();
@@ -13,7 +14,15 @@ async function CVPage() {
           <h1 className="m-0 -mt-1 font-bold text-cv-text dark:text-cv-text-dark">
             cv.pdf
           </h1>
-          <p className="m-0 -mt-1 text-xs">2 pages</p>
+          <p className="m-0 -mt-1 text-xs text-gray-400">2 pages</p>
+        </div>
+        <div className="flex items-center justify-end flex-1">
+          <a href="/documents/cv.pdf" download="marion-menzl-cv.pdf">
+            <LuDownload className="w-5 h-5 mr-6 text-text-light dark:text-text-dark" />
+          </a>
+          <a href="/documents/cv.pdf" target="_blank">
+            <LuExternalLink className="w-5 h-5 mr-6 text-text-light dark:text-text-dark" />
+          </a>
         </div>
       </div>
       <div className="flex-1 overflow-scroll">
