a27007e5fc68a3a02008eb31cbc5ee48dbcd0a6d
Marion Menzl <marion.menzl@me.com>
2023-09-18 10:58:17 +0200

    Add mobile support for desktop

--- components/Desktop/index.js
+++ components/Desktop/index.js
@@ -4,9 +4,9 @@ import { FcDocument } from "react-icons/fc";
 
 const Desktop = ({ children }) => {
   return (
-    <div className="relative flex-1">
-      <div className=" p-[30px 15px] sm:p-[15px]">
-        <div className="flex flex-wrap content-start w-full h-full sm:justify-start sm:flex-col sm:flex-wrap-reverse">
+    <div className="w-full h-full sm:relative sm:flex-1">
+      <div className="p-[30px_30px] sm:p-[15px]">
+        <div className="flex flex-wrap justify-between w-full h-full sm:content-start sm:justify-start sm:flex-col sm:flex-wrap-reverse">
           <DesktopIcon title="cv.pdf" location="/cv">
             <FcDocument className="w-full h-full" />
           </DesktopIcon>
