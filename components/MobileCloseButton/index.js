51d8d3a133520b2d2942a4c05a59091599ceba3c
Marion Menzl <marion.menzl@me.com>
2023-09-20 00:34:23 +0200

    Make close cross white

--- components/MobileCloseButton/index.js
+++ components/MobileCloseButton/index.js
@@ -6,7 +6,7 @@ function MobileCloseButton() {
     <div className="absolute w-12 h-12 bottom-7 right-7 sm:hidden">
       <Link href="/">
         <div className="flex items-center justify-center w-full h-full rounded-full bg-opacity-60 bg-neutral-400">
-          <IoMdClose className="text-3xl text-opacity-60" />
+          <IoMdClose className="text-3xl text-white text-opacity-60" />
         </div>
       </Link>
     </div>
