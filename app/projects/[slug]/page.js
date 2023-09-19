e53deb730e4235115f27be9f576e6e3143d9cd03
Marion Menzl <marion.menzl@me.com>
2023-09-19 22:00:12 +0200

    Fix scroll for static header bar and add bottom padding for close button

--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -30,38 +30,40 @@ const ProjectPage = async ({ params }) => {
   };
 
   return (
-    <div className="flex flex-col h-full overflow-y-scroll">
+    <div className="flex flex-col h-full ">
       <TopBar title={project.name} icon={project.icon} />
-      <Image
-        src={project.coverImageUrl}
-        alt={project.coverImageAlt}
-        width={800}
-        height={300}
-        className="w-full min-h-[300px] object-cover"
-      />
-      <div className="flex justify-center flex-1 w-full">
-        <div className="max-w-[800px] mx-8 pb-12">
-          <p className="mb-12 -mt-8 text-6xl">{project.icon}</p>
-          <Title title={project.name} date={project.date} />
-          <p className="italic">{project.tagline}</p>
-          <PortableText value={project.description} components={components} />
-          <div className="mt-4">
-            <Subtitle title="Technologies" />
-            {project.technologies?.map((technology, i) => (
-              <span
-                key={i}
-                className="inline-block px-2 py-1 mb-2 mr-2 text-black rounded bg-slate-300"
-              >
-                {technology}
-              </span>
-            ))}
-          </div>
-          {project.images?.length > 0 ? (
+      <div className="flex-1 pb-16 overflow-y-scroll">
+        <Image
+          src={project.coverImageUrl}
+          alt={project.coverImageAlt}
+          width={800}
+          height={300}
+          className="w-full min-h-[300px] object-cover"
+        />
+        <div className="flex justify-center flex-1 w-full">
+          <div className="max-w-[800px] mx-8 pb-12">
+            <p className="mb-12 -mt-8 text-6xl">{project.icon}</p>
+            <Title title={project.name} date={project.date} />
+            <p className="italic">{project.tagline}</p>
+            <PortableText value={project.description} components={components} />
             <div className="mt-4">
-              <Subtitle title="Impressions" />
-              <Gallery photos={getImages(project.images)} />
+              <Subtitle title="Technologies" />
+              {project.technologies?.map((technology, i) => (
+                <span
+                  key={i}
+                  className="inline-block px-2 py-1 mb-2 mr-2 text-black rounded bg-slate-300"
+                >
+                  {technology}
+                </span>
+              ))}
             </div>
-          ) : null}
+            {project.images?.length > 0 ? (
+              <div className="mt-4">
+                <Subtitle title="Impressions" />
+                <Gallery photos={getImages(project.images)} />
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
