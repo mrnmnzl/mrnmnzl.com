782db4884a57fb9991d02108ec7bddbf16cf8fe8
Marion Menzl <marion.menzl@me.com>
2023-09-19 21:56:57 +0200

    Replace flex with grid to match desktop layout on mobile

--- components/Dock/index.js
+++ components/Dock/index.js
@@ -5,7 +5,7 @@ import { FcMultipleDevices, FcInfo, FcSms, FcSettings } from "react-icons/fc";
 const Dock = () => {
   return (
     <footer className="relative flex-c-c w-full z-[1000] sm:h-[75px]">
-      <div className="dock bg-dock-mobile dark:bg-dock-mobile-dark sm:bg-dock dark:sm:bg-dock-dark">
+      <div className="grid grid-cols-4 justify-items-center sm:flex dock bg-dock-mobile dark:bg-dock-mobile-dark sm:bg-dock dark:sm:bg-dock-dark">
         <AppIcon location="/projects" text="Projects">
           <FcMultipleDevices className="w-full h-full text-gray-800" />
         </AppIcon>
