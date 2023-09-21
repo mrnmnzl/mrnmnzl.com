ac69583dc8aceb91a81a9ca3270efc14f447ee41
Marion Menzl <marion.menzl@me.com>
2023-09-21 17:53:27 +0200

    Fix light font color

--- app/mail/page.js
+++ app/mail/page.js
@@ -1,6 +1,6 @@
 function MailPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full text-neutral-600">
+    <div className="flex flex-col items-center justify-center w-full h-full text-neutral-400 dark:text-neutral-600">
       <p className="text-4xl font-light ">No Message Selected</p>
       <p className="mt-3">Use the button in top left to contact me</p>
     </div>
