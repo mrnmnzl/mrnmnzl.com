f2a4ae7aa779570f8df06c09411c45a2eb567ed5
Marion Menzl <marion.menzl@me.com>
2023-09-21 17:52:42 +0200

    Fix scroll on project page

--- app/projects/page.js
+++ app/projects/page.js
@@ -9,7 +9,7 @@ async function ProjectsPage() {
   const projects = await fetchProjects();
 
   return (
-    <div className="flex sm:block">
+    <div className="flex h-full sm:block">
       <div className="sm:hidden border-r-[1px] backdrop-blur border-window-border-light bg-projects-sidebar dark:bg-projects-sidebar-dark">
         <ProjectsLinksList
           projects={projects}
@@ -19,8 +19,8 @@ async function ProjectsPage() {
       </div>
       <div className="flex flex-col h-full">
         <ProjectsTopBar title={data.title} icon={data.icon} />
-        <div className="flex flex-col items-center flex-1 w-full pb-16">
-          <div className="max-w-[800px] w-full h-full overflow-scroll px-8 pb-12 mt-28">
+        <div className="flex flex-col items-center flex-1 w-full pb-16 overflow-scroll">
+          <div className="max-w-[800px] w-full px-8 pb-12 mt-28">
             <h1 className="mb-4 text-4xl font-bold">{data.title}</h1>
             <CustomPortableText data={data.introduction} />
             <CustomPortableText data={data.experience} />
