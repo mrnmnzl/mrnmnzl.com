77a1cc89434f62bed2dcb31cad6c9e38bb5f687c
Marion Menzl <marion.menzl@me.com>
2023-09-24 12:29:02 +0200

    Update metadata

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,20 @@ import "./globals.css";
 export const metadata = {
   title: "mrnmnzl Portfolio",
   description: "A personal portfolio by Marion Menzl",
+  openGraph: {
+    title: "MRNMNZL Portfolio",
+    description: "A personal portfolio by Marion Menzl",
+    url: "https://mrnmnzl.com",
+    siteName: "MRNMNZL Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MRNMNZL Portfolio",
+    description: "A personal portfolio by Marion Menzl",
+    creator: "@mrnmnzl",
+  },
 };
 
 function RootLayout({ children }) {
